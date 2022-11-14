ac44db77de086beac099fc7ae92609c3d7bf2f56
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-11-14 19:49:43 +0530

    Create district.js

---
+++ public/js/district.js
@@ -0,0 +1,68 @@
+function district(i)
+{
+    document.getElementById("dehradun").style="display:none;";
+    document.getElementById("haridwar").style="display:none;";
+    document.getElementById("rudraprayag").style="display:none;";
+    document.getElementById("almora").style="display:none;";
+    document.getElementById("nanital").style="display:none;";
+    document.getElementById("pithoragarh").style="display:none;";
+    document.getElementById("champawat").style="display:none;";
+    document.getElementById("tehri").style="display:none;";
+    document.getElementById("pauri").style="display:none;";
+    document.getElementById("uttarkashi").style="display:none;";
+    document.getElementById("udhamsinghnagar").style="display:none;";
+    document.getElementById("bageshwar").style="display:none;";
+    document.getElementById("chamoli").style="display:none;";
+    if(i==1)
+    {
+        document.getElementById("dehradun").style="display:block;";
+    }
+    else if(i==2)
+    {
+        document.getElementById("haridwar").style="display:block;";
+    }
+    else if(i==3)
+    {
+        document.getElementById("rudraprayag").style="display:block;";
+    }
+    else if(i==4)
+    {
+        document.getElementById("almora").style="display:block;";
+    }
+    else if(i==5)
+    {
+        document.getElementById("nanital").style="display:block;";
+    }
+    else if(i==6)
+    {
+        document.getElementById("pithoragarh").style="display:block;";
+    }
+    else if(i==7)
+    {
+        document.getElementById("champawat").style="display:block;";
+    }
+    else if(i==8)
+    {
+        document.getElementById("tehri").style="display:block;";
+    }
+    else if(i==9)
+    {
+        document.getElementById("pauri").style="display:block;";
+    }
+    else if(i==10)
+    {
+        document.getElementById("uttarkashi").style="display:block;";
+    }
+    else if(i==11)
+    {
+        document.getElementById("udhamsinghnagar").style="display:block;";
+    }
+    else if(i==12)
+    {
+        document.getElementById("bageshwar").style="display:block;";
+    }
+    else
+    {
+        document.getElementById("chamoli").style="display:block;";
+    }
+}
